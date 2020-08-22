db8c8b68efff9fccf0e608d3f29734270f5ae360
Quincy <q.baljeu@gmail.com>
2020-08-22 16:12:28 +0200

    Added project

---
+++ src/main/java/com/example/springbootslo/SpringBootSloApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootslo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSloApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootslo/SpringBootSloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootslo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
