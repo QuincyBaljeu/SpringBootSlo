4da2df88519dbd170f7b97e602bfa2c15b9021fd
Quincy <q.baljeu@gmail.com>
2020-08-30 20:01:36 +0200

    Added test method, not working

--- src/test/java/com/example/springbootslo/dataAccess/PharmacyDataAccessTest.java
+++ src/test/java/com/example/springbootslo/dataAccess/PharmacyDataAccessTest.java
@@ -1,8 +1,12 @@
 package com.example.springbootslo.dataAccess;
 
+import com.example.springbootslo.model.Appointment;
 import com.example.springbootslo.model.Doctor;
+import com.example.springbootslo.model.Patient;
+import com.example.springbootslo.util.AppointmentUtilities;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -12,34 +16,54 @@ import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.time.LocalDate;
+import java.util.UUID;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(PharmacyDataAccess.class)
 class PharmacyDataAccessTest {
 
-
+    @Autowired
     private MockMvc mvc;
 
     @Test
-    void addDoctor() throws Exception {
-
-
-    }
-
-    @Test
-    void addPatient() {
-    }
-
-    @Test
-    void addAppointment() {
-    }
-
-    @Test
     void updateAppointmentInfo() {
+
+        //BUG WERKT NIET
+
+        RequestBuilder appointmentPostBuilder = MockMvcRequestBuilders.post("api/appointment");
+
+        UUID appointmentId = UUID.randomUUID();
+        Doctor testDoctor = new Doctor("Quincy Baljeu");
+        Patient testPatient = new Patient("Jan Janssen");
+
+        Appointment testAppointment = new Appointment("2020-08-30");
+
+        try {
+            mvc.perform(appointmentPostBuilder).andReturn();
+        } catch (Exception e){
+            System.out.println("INVALID URL");
+            return;
+        }
+
+        RequestBuilder appointmentPutBuilder = MockMvcRequestBuilders.put("api/appointment" + appointmentId +"/" + testDoctor.getId() + "/" + testPatient.getId() + "TEST");
+
+        try {
+            mvc.perform(appointmentPutBuilder).andReturn();
+        } catch (Exception e){
+            System.out.println("INVALID URL");
+            return;
+        }
+
+        boolean check = false;
+
+        //Weet niet hoe ik een object uit het request kan halen.
     }
 
     @Test
     void updateAppointmentActivity() {
+        //Zelfde probleem ,Weet niet hoe ik een object uit het request kan halen.
     }
 }
